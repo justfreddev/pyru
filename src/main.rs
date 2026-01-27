44c594e98ef51c06219fd02f30c2609b492e76cc
Fred Padgham <mr.f.padgham@gmail.com>
2026-01-27 20:52:04 +0000

    Changed from shuttle to normal rocket

--- src/main.rs
+++ src/main.rs
@@ -41,9 +41,9 @@ mod value;
 mod tests;
 
 #[allow(unused)]
-use rocket::{http::Method, launch, post, routes};
-use rocket::serde::{Deserialize, Serialize, json::Json};
-use rocket_cors::{AllowedHeaders, AllowedOrigins, Cors, CorsOptions};
+use rocket::{ http::Method, launch, post, routes };
+use rocket::serde::{ Deserialize, Serialize, json::Json };
+use rocket_cors::{ AllowedHeaders, AllowedOrigins, Cors, CorsOptions };
 use std::io::Write;
 
 use run::run;
@@ -53,46 +53,47 @@ struct Message {
     source: String,
 }
 
-fn _repl() -> String {
-    let mut source = String::new();
-    loop {
-        let mut temp_source = String::new();
-        print!("> ");
-        std::io::stdout().flush().unwrap();
-        std::io::stdin().read_line(&mut temp_source).unwrap();
-        if temp_source.trim().eq("run") || temp_source.trim().eq("") {
-            return source
-                .chars()
-                .collect::<Vec<char>>()[0..source.len()-3]
-                .iter()
-                .collect::<String>();
-        }
-        temp_source.push('\n');
-        source.push_str(&temp_source);
-    }
-}
-
+// fn _repl() -> String {
+//     let mut source = String::new();
+//     loop {
+//         let mut temp_source = String::new();
+//         print!("> ");
+//         std::io::stdout().flush().unwrap();
+//         std::io::stdin().read_line(&mut temp_source).unwrap();
+//         if temp_source.trim().eq("run") || temp_source.trim().eq("") {
+//             return source
+//                 .chars()
+//                 .collect::<Vec<char>>()[0..source.len()-3]
+//                 .iter()
+//                 .collect::<String>();
+//         }
+//         temp_source.push('\n');
+//         source.push_str(&temp_source);
+//     }
+// }
 
 fn make_cors() -> Cors {
-    let allowed_origins = AllowedOrigins::some_exact(&[ // 4.
-        "http://localhost:8080",
-        "http://127.0.0.1:8080",
-        "http://localhost:8000",
-        "http://0.0.0.0:8000",
-        "http://localhost:5173"
-    ]);
+    let allowed_origins = AllowedOrigins::some_exact(
+        &[
+            "http://localhost:8080",
+            "http://127.0.0.1:8080",
+            "http://localhost:8000",
+            "http://0.0.0.0:8000",
+            "http://localhost:5173",
+            "https://pyru-playground.vercel.app",
+        ]
+    );
 
-    CorsOptions {
+    (CorsOptions {
         allowed_origins,
         allowed_methods: vec![Method::Post].into_iter().map(From::from).collect(),
         allowed_headers: AllowedHeaders::all(),
         allow_credentials: true,
         ..Default::default()
-    }
-    .to_cors()
-    .expect("error while building CORS")
+    })
+        .to_cors()
+        .expect("error while building CORS")
 }
-
 
 #[post("/runcode", format = "json", data = "<message>")]
 fn run_code(message: Json<Message>) -> Json<String> {
@@ -102,17 +103,15 @@ fn run_code(message: Json<Message>) -> Json<String> {
     Json(format!("{:?}", output))
 }
 
-
 #[launch]
-fn rocket() -> _ {
+async fn rocket() -> _ {
     rocket::build().mount("/v1", routes![run_code]).attach(make_cors())
 }
 
+// fn _main() {
+//     let source = _repl();
 
-fn _main() {
-    let source = _repl();
+//     let debug = false;
 
-    let debug = false;
-
-    let _ = run(source.as_str(), debug);
-}
+//     let _ = run(source.as_str(), debug);
+// }
